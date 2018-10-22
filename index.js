4a9695bef47f2b17c069de667b73b894f055f994
Bill DePhillips <wdephill@amazon.com>
2018-10-22 11:22:24 -0700

    implemented rankUserReviewsWithRecency and scoreReviewWithRecency

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+const { DateTime } = require('luxon');
+
+const ageInWeeks = (review) => {
+  if (!review.createdAt) {
+    return 0;
+  }
+  const createdAt = DateTime.fromISO(review.createdAt);
+  const diff = DateTime.local().diff(createdAt, 'weeks');
+  return Math.floor(diff.weeks);
+};
 
 const scoreReview = (review) => review.likeCount + review.reviewer.followerCount;
+
+const scoreReviewWithRecency = (review) => scoreReview(review) - ageInWeeks(review);
 
 const rankUserReviews = (reviews) => {
   const copy = [].concat(reviews);
   return copy.sort((a, b) => scoreReview(b) - scoreReview(a));
 };
 
+const rankUserReviewsWithRecency = (reviews) => {
+  const copy = [].concat(reviews);
+  return copy.sort((a, b) => scoreReviewWithRecency(b) - scoreReviewWithRecency(a));
+};
+
 module.exports = {
   scoreReview,
-  rankUserReviews
+  scoreReviewWithRecency,
+  rankUserReviews,
+  rankUserReviewsWithRecency,
 };
--- test/index_test.js
+++ test/index_test.js
@@ -1,8 +1,14 @@
 const assert = require('assert');
-const { scoreReview, rankUserReviews } = require('../index');
+const { scoreReview,
+  rankUserReviews,
+  scoreReviewWithRecency,
+  rankUserReviewsWithRecency,
+} = require('../index');
+const { DateTime } = require('luxon');
 
 const DEFAULT_REVIEW = {
   likeCount: 0,
+  createdAt: new DateTime.local(),
   reviewer: {
     followerCount: 0
   }
@@ -25,11 +31,38 @@ describe("scoreReview", () => {
 
 });
 
+describe("scoreReviewWithRecency", () => {
+
+  it("should be higher when likeCount is higher", () => {
+    const moreLikedReview = createReview({likeCount: 2});
+    const lessLikedReview = createReview({likeCount: 1});
+    assert(scoreReviewWithRecency(moreLikedReview) > scoreReviewWithRecency(lessLikedReview));
+  });
+
+    
+  it("should be lower when review is older", () => {
+    const newerReview = createReview({createdAt: '2018-09-26'});
+    const olderReview = createReview({ createdAt: '2018-09-01' });
+    assert(scoreReviewWithRecency(newerReview) > scoreReviewWithRecency(olderReview));
+  })
+
+});
+
 describe("rankUserReviews", () => {
   it("should rank more likes higher", () => {
     const moreLikedReview = createReview({likeCount: 2});
     const lessLikedReview = createReview({ likeCount: 1 });
     const ranked = rankUserReviews([lessLikedReview, moreLikedReview]);
     assert.deepStrictEqual(ranked, [moreLikedReview, lessLikedReview]);
+  });
+});
+
+describe.only("rankUserReviewsWithRecency", () => {
+  it("should rank recently created higher than older with more likes", () => {
+    const twoWeeksAgo = new DateTime.local().minus({ weeks: 2 });
+    const olderReviewWithMoreLikes = createReview({likeCount: 2, createdAt: twoWeeksAgo});
+    const lessLikedButNewerReview = createReview({ likeCount: 1, createdAt: new DateTime.local() });
+    const ranked = rankUserReviewsWithRecency([olderReviewWithMoreLikes, lessLikedButNewerReview]);
+    assert.deepStrictEqual(ranked, [lessLikedButNewerReview, olderReviewWithMoreLikes]);
   });
 });
