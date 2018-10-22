9462655531e5797198726426da012c3cb0fb68ee
Bill DePhillips <wdephill@amazon.com>
2018-10-22 11:29:44 -0700

    added example possible low-value code paths

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 
-const scoreReview = (review) => review.likeCount + review.reviewer.followerCount;
+const scoreReview = (review) => {
+  // scaling fix. TODO clean up etc etc
+  if (ShedReviewScoring.isEnabled()) {
+    return 1;
+  }
+
+  const friendLikes = Liker.isFriend(review.likes).length;
+  const nonFriendLikes = Liker.isNonFriend(review.likes).length;
+
+  let score = (friendLikes * 0.75)
+    + nonFriendLikes
+    + review.reviewer.followerCount;
+  
+  if (InfluencerProgram.isParticipant(review.reviewer)) {
+    score += 10;
+  };
+  
+  if (review.reviewer.accountAge < 10) {
+    score -= 5;
+  }
+  
+  if (review.reviewer.friendCount < 10) {
+    score += 5;
+  }
+  return score;
+};
 
 const rankUserReviews = (reviews) => {
   const copy = [].concat(reviews);
